4d4f1949f5b9200be55621af1933fcea037b012d
siyed5005-boop <siyed5005@gmail.com>
2025-11-28 21:37:51 +0100

    iyed

--- src/main/java/tn/esprit/spring/control/SecurityTestController.java
+++ src/main/java/tn/esprit/spring/control/SecurityTestController.java
@@ -51,6 +51,12 @@ public class SecurityTestController {
    // Variables sensibles en dur
    
    // ❌ TEST : Secrets exposés
+     // Variables sensibles en dur
+   
+   // ❌ TEST : Secrets exposés
+   // Variables sensibles en dur
+      // ❌ TEST : Secrets exposés
+   // Variables sensibles en dur
    // Variables sensibles en dur
       // ❌ TEST : Secrets exposés
    // Variables sensibles en dur
