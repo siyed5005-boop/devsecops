3fd35a1a839ec7ff2267a3272c1d7f40bdeaa9eb
siyed5005-boop <siyed5005@gmail.com>
2025-11-28 19:16:38 +0100

    iyed

--- src/main/java/tn/esprit/spring/control/SecurityTestController.java
+++ src/main/java/tn/esprit/spring/control/SecurityTestController.java
@@ -6,6 +6,8 @@ import java.sql.*;
 @RestController
 @RequestMapping("/security-test")
 public class SecurityTestController {
+   // ❌ INJECTION SQL TRÈS EXPLICITE
+   // ❌ INJECTION SQL TRÈS EXPLICITE
 
    // ❌ INJECTION SQL TRÈS EXPLICITE
    @GetMapping("/login-unsafe")
